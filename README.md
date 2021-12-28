21b221421599538de76e0548cffecd5f872a2d08
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-12-28 09:33:37 +0530

    modified:   README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # traefik
+
+Install yq:
+```bash
+VERSION=$(curl -s https://github.com/mikefarah/yq/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
+BINARY=yq_linux_amd64
+
+sudo wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/local/bin/yq
+sudo chmod +x /usr/local/bin/yq
+```
+
+http://127.0.0.1:8080/dashboard/
+
+Start traefik server:
+```bash
+docker-compose up -d --force-recreate
+```
---
+++ generate-dynamic-conf.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TRAEFIK_DYNAMIC_CONFIG_FILE=dynamic-conf.yaml
+rm ${TRAEFIK_DYNAMIC_CONFIG_FILE}
+touch ${TRAEFIK_DYNAMIC_CONFIG_FILE}
+
+while IFS= read -r line; do
+  IP=$(echo ${line} | awk '{print $1}')
+  DOMAIN=$(echo ${line} | awk '{print $2}')
+  ROUTER=router-${IP//./-}
+  SERVICE=service-${IP//./-}
+
+  #  http - router
+  YQ_CMD="yq e '.http.routers.${ROUTER}.entryPoints[0] = \"web\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.http.routers.${ROUTER}.rule = \"Host(\`${DOMAIN}\`)\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.http.routers.${ROUTER}.service = \"${SERVICE}\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+
+  # http - service
+  YQ_CMD="yq e '.http.services.${SERVICE}.loadBalancer.servers[0].url = \"http://${IP}\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+
+  # tcp - router
+  YQ_CMD="yq e '.tcp.routers.${ROUTER}.entryPoints.[0] = \"websecure\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.tcp.routers.${ROUTER}.rule = \"HostSNI(\`${DOMAIN}\`)\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.tcp.routers.${ROUTER}.service = \"${SERVICE}\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+
+  # tcp - service
+  YQ_CMD="yq e '.tcp.services.${SERVICE}.loadBalancer.servers[0].address = \"${IP}:443\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+
+done < hosts
