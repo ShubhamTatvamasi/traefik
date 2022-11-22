334082e420aaaaa25a0761b71b05d67cf7f03f6f
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-11-22 16:34:35 +0530

    modified:   generate-dynamic-conf.sh

--- generate-dynamic-conf.sh
+++ generate-dynamic-conf.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+TEMP_DYNAMIC_CONFIG_FILE=/tmp/conf.yaml
 TRAEFIK_DYNAMIC_CONFIG_FILE=dynamic/conf.yaml
-echo -n "" > ${TRAEFIK_DYNAMIC_CONFIG_FILE}
+echo -n "" > ${TEMP_DYNAMIC_CONFIG_FILE}
 
 while read line; do
   # If the line is empty, skip it
@@ -13,28 +14,30 @@ while read line; do
   SERVICE=service-${IP//./-}
 
   # http and tcp - entryPoints
-  YQ_CMD="yq e '.http.routers.${ROUTER}.entryPoints[0] = \"web\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
-  YQ_CMD="yq e '.tcp.routers.${ROUTER}.entryPoints.[0] = \"websecure\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.http.routers.${ROUTER}.entryPoints[0] = \"web\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.tcp.routers.${ROUTER}.entryPoints.[0] = \"websecure\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
 
   # http and tcp - rule
   if [ "${DOMAIN:0:1}" == "*" ]; then
     DOMAIN=${DOMAIN:2}
-    YQ_CMD="yq e '.http.routers.${ROUTER}.rule = \"HostRegexp(\`{subdomain:.+}.${DOMAIN}\`)\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
-    YQ_CMD="yq e '.tcp.routers.${ROUTER}.rule = \"HostSNIRegexp(\`{subdomain:.+}.${DOMAIN}\`)\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+    YQ_CMD="yq e '.http.routers.${ROUTER}.rule = \"HostRegexp(\`{subdomain:.+}.${DOMAIN}\`)\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+    YQ_CMD="yq e '.tcp.routers.${ROUTER}.rule = \"HostSNIRegexp(\`{subdomain:.+}.${DOMAIN}\`)\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
   else
-    YQ_CMD="yq e '.http.routers.${ROUTER}.rule = \"Host(\`${DOMAIN}\`)\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
-    YQ_CMD="yq e '.tcp.routers.${ROUTER}.rule = \"HostSNI(\`${DOMAIN}\`)\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+    YQ_CMD="yq e '.http.routers.${ROUTER}.rule = \"Host(\`${DOMAIN}\`)\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+    YQ_CMD="yq e '.tcp.routers.${ROUTER}.rule = \"HostSNI(\`${DOMAIN}\`)\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
   fi
 
   # http and tcp - service
-  YQ_CMD="yq e '.http.routers.${ROUTER}.service = \"${SERVICE}\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
-  YQ_CMD="yq e '.tcp.routers.${ROUTER}.service = \"${SERVICE}\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.http.routers.${ROUTER}.service = \"${SERVICE}\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.tcp.routers.${ROUTER}.service = \"${SERVICE}\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
 
   # http and tcp - url and address
-  YQ_CMD="yq e '.http.services.${SERVICE}.loadBalancer.servers[0].url = \"http://${IP}\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
-  YQ_CMD="yq e '.tcp.services.${SERVICE}.loadBalancer.servers[0].address = \"${IP}:443\"' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.http.services.${SERVICE}.loadBalancer.servers[0].url = \"http://${IP}\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.tcp.services.${SERVICE}.loadBalancer.servers[0].address = \"${IP}:443\"' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
 
   # tcp - tls passthrough
-  YQ_CMD="yq e '.tcp.routers.${ROUTER}.tls.passthrough = true' -i ${TRAEFIK_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
+  YQ_CMD="yq e '.tcp.routers.${ROUTER}.tls.passthrough = true' -i ${TEMP_DYNAMIC_CONFIG_FILE}"; eval ${YQ_CMD}
 
 done < hosts
+
+cp ${TEMP_DYNAMIC_CONFIG_FILE} ${TRAEFIK_DYNAMIC_CONFIG_FILE}
